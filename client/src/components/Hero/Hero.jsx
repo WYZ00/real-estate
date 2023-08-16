a68b5ef849753c55246105e33eb8d45c9be2a0c2
WYZ00 <thanthtettin@gmail.com>
2023-08-16 22:30:02 +0630

    add initial animation to hero section

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
 import CountUp from "react-countup";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -11,13 +12,19 @@ const Hero = () => {
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle" />
-            <h1>
+            <motion.h1
+              initial={{ y: "2rem", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                duration: 2,
+                type: "spring",
+              }}>
               Discover
               <br />
               Most Suitable
               <br />
               Property
-            </h1>
+            </motion.h1>
           </div>
 
           <div className="flexColStart hero-des">
@@ -66,9 +73,16 @@ const Hero = () => {
 
         {/* right side */}
         <div className="flexCenter hero-right">
-          <div className="image-container">
+          <motion.div
+            initial={{ x: "7rem", opacity: 0 }}
+            animate={{ x: "0", opacity: 1 }}
+            transition={{
+              duration: 2,
+              type: "spring",
+            }}
+            className="image-container">
             <img src="./hero-image.png" alt="hero-image" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>

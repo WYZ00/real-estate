91efb3131063fb5be54432732dba188d7cfead0a
WYZ00 <thanthtettin@gmail.com>
2023-08-17 21:46:32 +0630

    making routes and reusable components

--- client/src/components/Residencies/Residencies.jsx
+++ client/src/components/Residencies/Residencies.jsx
@@ -3,6 +3,7 @@ import "./Residencies.css";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import data from "../../utils/slider.json";
 import { sliderSettings } from "../../utils/common";
+import PropertyCard from "../PropertyCard/PropertyCard";
 
 const Residencies = () => {
   return (
@@ -17,16 +18,7 @@ const Residencies = () => {
           <SliderButtons />
           {data.map((card, i) => (
             <SwiperSlide key={i}>
-              <div className="flexColStart r-card">
-                <img src={card.image} alt={card.name} />
-                <span className="secondaryText r-price">
-                  <span style={{ color: "orange" }}>$</span>
-                  <span>{card.price}</span>
-                </span>
-
-                <span className="primaryText">{card.name}</span>
-                <span className="secondaryText">{card.detail}</span>
-              </div>
+              <PropertyCard card={card} />
             </SwiperSlide>
           ))}
         </Swiper>
